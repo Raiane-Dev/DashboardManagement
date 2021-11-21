f271474aa68603d8f6e26b7e7b7c352d58b4f8b3
Raiane Dev <89032013+Raiane-Dev@users.noreply.github.com>
2021-11-21 10:57:55 -0300

    Update

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     date_default_timezone_set('America/Sao_Paulo');
-    define('INCLUDE_PATH','http://localhost/Curso/Projeto/DashboardManagement/');
+    define('INCLUDE_PATH','http://localhost/DashboardManagement/');
 
     define('HOST','localhost');
     define('DATABASE', 'dashboard_management');
